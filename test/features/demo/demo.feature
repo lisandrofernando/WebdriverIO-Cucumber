5e06b7fbf428f77a627e499471047534d935f2bc
lisandrofernando <lisandrusfernandus@gmail.com>
2022-03-10 17:04:52 -0600

    first commit

---
+++ test/features/demo/demo.feature
@@ -0,0 +1,13 @@
+Feature: Google Search
+
+    @demo
+    Scenario Outline: Searching for wdio
+        Given Google url is open
+        When Search with <searchItem>
+        Then Click on first search result
+        Then URL should match <ExpectedURL>
+
+        Examples:
+            | TestID | searchItem | ExpectedURL           |
+            | Demo   | wdio       | https://webdriver.io/ |
+
---
+++ test/features/step-definitions/demo.ts
@@ -0,0 +1,25 @@
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import chai from "chai";
+
+Given(/^Google url is open$/, async function () {
+  browser.url("https://www.google.com.mx/");
+  browser.pause(6000);
+});
+
+When(/^Search with (.*)$/, async function (searchItem) {
+  console.log(`>>> the item: ${searchItem}`);
+  let elementSearch = await $(`[name=q]`);
+  await elementSearch.setValue(searchItem);
+  await browser.keys("Enter");
+});
+
+Then(/^Click on first search result$/, async function () {
+  let ele = await $(`<h3>`);
+  ele.click();
+});
+
+Then(/^URL should match (.*)$/, async function (ExpectedURL) {
+  console.log(`>> expectedURL:${ExpectedURL}`);
+  let url = await browser.getUrl();
+  chai.expect(url).to.equal(ExpectedURL);
+});
